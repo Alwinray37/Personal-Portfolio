272642c30a78f8275c8d61aca6709283ef6b5b92
alwinray37 <alwinray37@gmail.com>
2024-10-23 18:05:52 -0700

    about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,6 +7,7 @@ function About() {
                 <div className={`${styles.col}`}>
                     <h1>About</h1>
                     <p>I’m a Computer Science student at California State University, Northridge, with a passion for software development and technology innovation. I hold two associate degrees from Santa Monica College, one in Web Development and another in General Science, giving me a well-rounded foundation in both the technical and analytical aspects of computing.</p>
+                    <h3>Experience With:</h3>
                 </div>
                 <div className={`${styles.imgContainer}`}>
                     <img src="" alt="" />
